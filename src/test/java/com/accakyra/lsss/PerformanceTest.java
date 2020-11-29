b95d9945295b4efcaec53631d11f0facb4a56220
Eugene Mitikari <accakyra@inbox.ru>
2020-11-29 20:46:06 +0700

    add performance test

---
+++ src/test/java/com/accakyra/lsss/PerformanceTest.java
@@ -0,0 +1,53 @@
+package com.accakyra.lsss;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PerformanceTest extends TestBase {
+
+    void writePerformance() throws IOException {
+        int recordCount = 1_000_000;
+        Map<ByteBuffer, ByteBuffer> records = new HashMap<>();
+        for (int i = 0; i < recordCount; i++) {
+            records.put(randomKey(), randomBuffer(100));
+        }
+
+        long start;
+        try (DAO dao = createDao()) {
+            start = System.currentTimeMillis();
+            for (Map.Entry<ByteBuffer, ByteBuffer> record : records.entrySet()) {
+                dao.upsert(record.getKey(), record.getValue());
+            }
+        }
+        double time = (System.currentTimeMillis() - start) / 1000.;
+
+        System.out.println(time + " total time");
+        System.out.println(((16 + 100) * recordCount / (1024 * 1024) / time) + " MB per second");
+    }
+
+    void readPerformance() throws IOException {
+        int recordCount = 1_000_000;
+        Map<ByteBuffer, ByteBuffer> records = new HashMap<>();
+        for (int i = 0; i < recordCount; i++) {
+            records.put(randomKey(), randomBuffer(100));
+        }
+
+        long start;
+        try (DAO dao = createDao()) {
+            for (Map.Entry<ByteBuffer, ByteBuffer> record : records.entrySet()) {
+                dao.upsert(record.getKey(), record.getValue());
+            }
+            start = System.currentTimeMillis();
+            for (Map.Entry<ByteBuffer, ByteBuffer> record : records.entrySet()) {
+                dao.get(record.getKey());
+            }
+        }
+
+        double time = (System.currentTimeMillis() - start) / 1000.;
+
+        System.out.println(time + " total time");
+        System.out.println(((16 + 100) * recordCount / (1024 * 1024) / time) + " MB per second");
+    }
+}
