ca1b9a5866167dc057494bc1a469d3ed18776970
Eugene Mitikari <accakyra@inbox.ru>
2020-11-30 11:43:25 +0700

    fix time catching in performance test

--- src/test/java/com/accakyra/lsss/PerformanceTest.java
+++ src/test/java/com/accakyra/lsss/PerformanceTest.java
@@ -1,5 +1,7 @@
 package com.accakyra.lsss;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
@@ -7,6 +9,7 @@ import java.util.Map;
 
 public class PerformanceTest extends TestBase {
 
+    @Test
     void writePerformance() throws IOException {
         int recordCount = 1_000_000;
         Map<ByteBuffer, ByteBuffer> records = new HashMap<>();
@@ -27,6 +30,7 @@ public class PerformanceTest extends TestBase {
         System.out.println(((16 + 100) * recordCount / (1024 * 1024) / time) + " MB per second");
     }
 
+    @Test
     void readPerformance() throws IOException {
         int recordCount = 1_000_000;
         Map<ByteBuffer, ByteBuffer> records = new HashMap<>();
@@ -34,20 +38,19 @@ public class PerformanceTest extends TestBase {
             records.put(randomKey(), randomBuffer(100));
         }
 
-        long start;
         try (DAO dao = createDao()) {
             for (Map.Entry<ByteBuffer, ByteBuffer> record : records.entrySet()) {
                 dao.upsert(record.getKey(), record.getValue());
             }
-            start = System.currentTimeMillis();
+
+            long start = System.currentTimeMillis();
             for (Map.Entry<ByteBuffer, ByteBuffer> record : records.entrySet()) {
                 dao.get(record.getKey());
             }
+            double time = (System.currentTimeMillis() - start) / 1000.;
+
+            System.out.println(time + " total time");
+            System.out.println(((16 + 100) * recordCount / (1024 * 1024) / time) + " MB per second");
         }
-
-        double time = (System.currentTimeMillis() - start) / 1000.;
-
-        System.out.println(time + " total time");
-        System.out.println(((16 + 100) * recordCount / (1024 * 1024) / time) + " MB per second");
     }
 }
